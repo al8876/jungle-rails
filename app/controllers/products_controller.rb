21902adf3016921ec18996c0301daa954db7d4fc
al8876 <al8876@gmail.com>
2018-03-05 18:57:53 +0000

    Delete button works deleting specific review

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,26 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @reviews = Review.all.order(created_at: :desc)
   end
 
   def show
-    @product = Product.find(params[:id])
-    @review = Review.create
+    # @product = Product.find(params[:id])
+    # @review = Review.create
+    @product = Product.find params[:id]
+    @reviews = Review.find_by_product_id params[:id]
+    @review = Review.new(reviews_params)
+  end
+
+  private
+
+  def reviews_params
+    params.permit(
+      :description,
+      :rating,
+      :product_id,
+      :user_id
+    )
   end
 
   #did not implement
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,12 +2,14 @@ class ReviewsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-    @review = Review.new(review_params)
+    # @review = Review.new(review_params)
+    # @review.user_id = current_user.id
+    # @review.product_id = @product.id
+    @review = @product.reviews.create(review_params)
     @review.user_id = current_user.id
-    @review.product_id = @product.id
     
     if @review.save
-      redirect_to "/products/#{@review.product_id}"
+      redirect_to @product
     else
       redirect_to "/login", notice: "Error: Review did not submit"
     end
