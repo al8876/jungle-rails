cc5b454e9b7cdca36af4177c8b301f9618e6f15d
al8876 <al8876@gmail.com>
2018-03-05 21:22:57 +0000

    specs created to test new product creation has validations

--- Gemfile
+++ Gemfile
@@ -47,6 +47,10 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.5'
 end
 
+group :test do
+  gem 'shoulda-matchers', '~> 3.0', require: false
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+# describe Product do
+#   it { is_expected.to validate_presence_of(:name) }
+#   it { is_expected.to validate_presence_of(:price) }
+#   it { is_expected.to validate_presence_of(:quantity) }
+#   it { is_expected.to validate_presence_of(:category_id) }
+# end
+
+RSpec.describe Product, type: :model do
+  
+  before :example do
+    @mug = Product.new name: 'mug', price_cents: 5, quantity: 10, category_id: 1
+  end
+
+  it 'Creates a new product record with name, price, quantity, and category' do
+    expect(@mug.valid?).to eq true
+  end
+
+  describe 'Validations' do
+    
+    it 'throws an error if name is missing' do
+      @mug.name = nil;
+      expect(@mug.valid?).to eq false
+    end
+
+    it 'throws an error if price is missing' do
+      @mug.price_cents = nil;
+      expect(@mug.valid?).to eq false
+    end
+
+    it 'throws an error if quantity is missing' do
+      @mug.quantity = nil;
+      expect(@mug.valid?).to eq false
+    end
+
+    it 'throws an error if category is missing' do
+      @mug.category_id = nil;
+      expect(@mug.valid?).to eq false
+    end
+
+  end
+end
